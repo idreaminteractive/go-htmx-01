0001fdbb91ba3b21f0fa6ee355b5524f5bc7f651
Dave Wiper <dwiper@idreaminteractive.com>
2023-08-23 03:18:57 +0000

    chore: saving

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"main/components"
 	"net/http"
 
+	// "github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	_ "github.com/mattn/go-sqlite3"
@@ -18,19 +19,19 @@ func check(err error) {
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
- component := components.Hello("Dave")
+	component := components.Hello("Dave")
 	// check(err)
-  
-  component.Render(context.Background(), w)
-	
+	// templ.Handler(component)
+	component.Render(context.Background(), w)
+
 }
 
 func main() {
 	// fmt.Printf("%s\n", views.Hello("Foo"))
 	// fmt.Printf("%s\n", views.Hello("potato"))
-  // component := Hello("dave")
+	// component := Hello("dave")
 	r := chi.NewRouter()
- r.Use(middleware.Logger)
+	r.Use(middleware.Logger)
 	r.Get("/", handleIndex)
 	http.ListenAndServe(":3000", r)
 
