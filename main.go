25226e6ebe20ae6bd3909c0602cdaa817fb4e809
Dave Wiper <dwiper@idreaminteractive.com>
2023-08-28 11:04:52 +0000

    chore: broken, but getting there

--- main.go
+++ main.go
@@ -27,7 +27,57 @@ func handleIndex(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+
 	r.Get("/", handleIndex)
+
 	http.ListenAndServe(":3000", r)
 
+}
+
+type IndexHandler struct {
+	Log      *log.Logger
+	GetIndex func() (IndexData, error)
+}
+
+func (ih IndexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ps, err := ih.GetIndex()
+	if err != nil {
+		ih.Log.Printf("failed to get index: %v", err)
+		http.Error(w, "failed to retrieve index", http.StatusInternalServerError)
+		return
+	}
+	templ.Handler(IndexTemplate(ps)).ServeHTTP(w, r)
+}
+
+// Actual data object for index of the site
+type IndexData struct {
+	name  string
+	posts []PostData
+}
+
+func NewIndexHandler() IndexHandler {
+	// get our data
+	getIndex := func() (IndexData, error) {
+		return IndexData{
+			name:  "Index",
+			posts: make([]PostData, 0),
+		}, nil
+
+	}
+	return IndexHandler{
+		Log:      log.Default(),
+		GetIndex: getIndex,
+	}
+}
+
+type PostHandler struct {
+	Log     *log.Logger
+	GetPost func(postId int) (PostData, error)
+}
+
+type PostData struct {
+	id      int
+	name    string
+	content string
 }
