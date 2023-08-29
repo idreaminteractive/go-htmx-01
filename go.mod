082b1e2fe98885d33ed3772cd408dd1379b93921
Dave Wiper <dwiper@idreaminteractive.com>
2023-08-29 10:41:37 +0000

    feat: gomponents is decent

--- go.mod
+++ go.mod
@@ -6,4 +6,8 @@ require github.com/go-chi/chi/v5 v5.0.10
 
 require github.com/a-h/templ v0.2.316
 
-require github.com/mattn/go-sqlite3 v1.14.17
+require (
+	github.com/maragudk/gomponents v0.20.1
+	github.com/maragudk/gomponents-htmx v0.3.0
+	github.com/mattn/go-sqlite3 v1.14.17
+)
--- main.go
+++ main.go
@@ -1,28 +1,71 @@
 package main
 
 import (
+	"errors"
 	"log"
-	"main/app"
 	"net/http"
+	"time"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	_ "github.com/mattn/go-sqlite3"
+	g "github.com/maragudk/gomponents"
+	hx "github.com/maragudk/gomponents-htmx"
+	hxhttp "github.com/maragudk/gomponents-htmx/http"
+	c "github.com/maragudk/gomponents/components"
+	. "github.com/maragudk/gomponents/html"
+	ghttp "github.com/maragudk/gomponents/http"
 )
 
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
+func main() {
+	if err := start(); err != nil {
+		log.Fatalln("Error:", err)
 	}
 }
 
-func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(middleware.RequestID)
+func start() error {
+	now := time.Now()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", ghttp.Adapt(func(w http.ResponseWriter, r *http.Request) (g.Node, error) {
+		if r.Method == http.MethodPost && hxhttp.IsBoosted(r.Header) {
+			now = time.Now()
 
-	r.Get("/", app.NewIndexHandler().ServeHTTP)
+			hxhttp.SetPushURL(w.Header(), "/?time="+now.Format(timeFormat))
 
-	http.ListenAndServe(":3000", r)
+			return partial(now), nil
+		}
+		return page(now), nil
+	}))
 
+	log.Println("Starting on http://localhost:8080")
+	if err := http.ListenAndServe("localhost:8080", mux); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	return nil
+}
+
+const timeFormat = "15:04:05"
+
+func page(now time.Time) g.Node {
+	return c.HTML5(c.HTML5Props{
+		Title: now.Format(timeFormat),
+		Head: []g.Node{
+			Script(Src("https://cdn.tailwindcss.com?plugins=forms,typography")),
+			Script(Src("https://unpkg.com/htmx.org")),
+		},
+		Body: []g.Node{
+			Div(Class("max-w-7xl mx-auto p-4 prose lg:prose-lg xl:prose-xl"),
+				Div(Class("text-lg"), g.Text("I am a new div")),
+				H1(g.Text(`gomponents + HTMX`)),
+				P(g.Textf(`Time at last full page refresh was %v.`, now.Format(timeFormat))),
+				partial(now),
+				FormEl(Method("post"), Action("/"), hx.Boost("true"), hx.Target("#partial"), hx.Swap("outerHTML"),
+					Button(Type("submit"), g.Text(`Update time`),
+						Class("rounded-md border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2"),
+					),
+				),
+			),
+		},
+	})
+}
+
+func partial(now time.Time) g.Node {
+	return P(ID("partial"), g.Textf(`Time was last updated at %v.`, now.Format(timeFormat)))
 }
