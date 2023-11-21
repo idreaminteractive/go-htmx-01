e160a4db7deb3124f6918ef3415a8b2fd9f96772
Dave Wiper <dwiper@idreaminteractive.com>
2023-11-21 11:11:16 +0000

    chore: added better way to interact with sqlite

--- scripts/seed.go
+++ scripts/seed.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"context"
 	"database/sql"
-	"fmt"
 	"log"
 
 	"main/internal/db"
 	"main/internal/services"
 	"main/internal/views/dto"
 
-	"github.com/go-faker/faker/v4"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	ctx := context.Background()
 
 	database, err := sql.Open("sqlite3", "/litefs/potato.db")
 	if err != nil {
@@ -25,22 +21,11 @@ func main() {
 	queries := db.New(database)
 
 	as := services.InitAuthService(&services.ServiceLocator{}, queries)
-	user, _ := as.Authenticate(dto.UserLoginDTO{Email: "dwiper@gmail.com", Password: "dave"})
 
-	item, err := queries.CreateNote(ctx, db.CreateNoteParams{UserID: user.ID, Content: faker.Paragraph(), IsPublic: false})
+	_, err = as.Register(dto.RegisterDTO{Handle: "Dave", Email: "dwiper@gmail.com", Password: "dave", ConfirmPassword: "dave"})
+
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Printf("%v", item)
-	// finished_todo, err := queries.CreateTodo(ctx, "Finished todo")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// err = queries.SetTodoDone(ctx, finished_todo.ID)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
 
 }
